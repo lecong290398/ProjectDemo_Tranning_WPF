f3c85b1ec04efc83e2236474f8c58210625d1fbe
NguyenHai <nguyenhai1449@gmai.com>
2019-11-07 11:17:57 +0700

    Upload code
    Create a simple data application with WPF and Entity Framework 6

---
+++ Soucre_Hai/Code/v1/DemoPrsim_Login/Connect/Data.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPrsim_Login.Connect
+{
+    public class Data
+    {
+        public static int executeQuery(string query)
+        {
+            // Initialization.  
+            int rowCount = 0;
+
+            string strConn = "Data Source=(LocalDb)\\LocalDB;Initial Catalog=QLNhanVien;Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(strConn);
+            SqlCommand sqlCommand = new SqlCommand();
+
+            try
+            {
+                // Settings.  
+                sqlCommand.CommandText = query;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandTimeout = 12 * 3600; //// Setting timeeout for longer queries to 12 hours.  
+
+                // Open.  
+                sqlConnection.Open();
+
+                // Result.  
+                rowCount = sqlCommand.ExecuteNonQuery();
+
+                // Close.  
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Close.  
+                sqlConnection.Close();
+
+                throw ex;
+            }
+
+            return rowCount;
+        }
+    }
+}
---
+++ Soucre_Hai/Code/v1/DemoPrsim_Login/Model/User.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPrsim_Login.Model
+{
+    public class User : BindableBase
+    {
+        #region Id
+        private string _Id = null;
+        public string Id
+        {
+            get { return _Id; }
+            set { SetProperty(ref _Id, value); }
+        }
+        #endregion
+
+        #region HoTen
+        private string _HoTen = null;
+        public string HoTen
+        {
+            get { return _HoTen; }
+            set { SetProperty(ref _HoTen, value); }
+        }
+        #endregion
+
+
+        #region MatKhau
+        private string _MatKhau = null;
+        public string MatKhau
+        {
+            get { return _MatKhau; }
+            set { SetProperty(ref _MatKhau, value); }
+        }
+        #endregion
+    }
+}
