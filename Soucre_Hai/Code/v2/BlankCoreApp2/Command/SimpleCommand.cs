deff5310c3390f42b4752f6309fa7d53ea74cbbf
NguyenHai <nguyenhai1449@gmai.com>
2019-11-12 17:10:26 +0700

    Them code 12/11/2019
    Them code 12/11/2019

---
+++ Soucre_Hai/Code/v2/BlankCoreApp2/Command/SimpleCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace BlankCoreApp2.Command
+{
+    public class SimpleCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+
+        public SimpleCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new NullReferenceException();
+            }
+            _execute = execute;
+            _canExecute = CanExecute;
+        }
+
+        public SimpleCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+
+        }
+    }
+}
---
+++ Soucre_Hai/Code/v2/BlankCoreApp2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using BlankCoreApp2.Command;
+using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace BlankCoreApp2.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        
+        public ObservableCollection<string> myMessages { get; set; }
+        public SimpleCommand MessageBoxCommand { get; private set; }
+        public SimpleCommand ConsoleLogCommand { get; private set; }
+        
+       
+
+        public MainWindowViewModel()
+        {
+            myMessages = new ObservableCollection<string>()
+            {
+                "My name is Hai",
+                "Nguyen Van B",
+                "Nguyen Van E",
+                "I am message box",
+                "I am console"
+            };
+
+            MessageBoxCommand = new SimpleCommand(DisplayInMessageBox, MessageCanUse);
+            ConsoleLogCommand = new SimpleCommand(DisplayInConsole, ConsoleCanUse);
+        }
+
+        public void DisplayInMessageBox(object message)
+        {
+            MessageBox.Show((string)message);
+        }
+
+        public void DisplayInConsole(object message)
+        {
+            Console.WriteLine((string)message);
+        }
+
+
+        public bool MessageCanUse(object message)
+        {
+            if ((string)message == "I am console")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool ConsoleCanUse(object message)
+        {
+            if ((string)message == "I am message box")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
